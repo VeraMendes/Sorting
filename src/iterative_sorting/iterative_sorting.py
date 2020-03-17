2e3545ff9d2e3a1f4ca51f92b12c2a3392801b10
VeraMendes <veramendes10@gmail.com>
2020-03-17 00:22:27 -0700

    mvp day1

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,26 +1,40 @@
-# TO-DO: Complete the selection_sort() function below 
+# Complete the selection_sort() function below 
 def selection_sort( arr ):
+
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
+        # curr_index = i
+        # smallest_index = curr_index
+        smallest_index = i
+        # find next smallest element
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+        
+            # (hint, can do in 3 loc) 
+    
+        # swap
+        if smallest_index != i:
+            # print(f'{i} {smallest_index} {arr[i]} {arr[smallest_index]}')
+            temp = arr[i]
+            arr[i] = arr[smallest_index]
+            arr[smallest_index] = temp
 
     return arr
 
 
-# TO-DO:  implement the Bubble Sort function below
+# Implement the Bubble Sort function below
 def bubble_sort( arr ):
 
+    for i in range(len(arr)):
+
+        for j in range(0, (len(arr)-i-1)):
+
+            if arr[j] > arr[j+1]:
+                # print(f'{j} {j+1} {arr[j]} {arr[j+1]}')
+                temp= arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp                
     return arr
 
 
